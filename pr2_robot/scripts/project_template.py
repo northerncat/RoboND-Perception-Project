cf884188b2f41ccc8fb8444e1872bf92fdcaf017
Brad Huang <brad820309@gmail.com>
2018-04-22 13:58:15 -0700

    Changing the sensor_stick subdir to a different name to avoid namespace collision

--- pr2_robot/scripts/project_template.py
+++ pr2_robot/scripts/project_template.py
@@ -299,7 +299,7 @@ if __name__ == '__main__':
     detected_objects_pub = rospy.Publisher("/detected_objects", DetectedObjectsArray, queue_size=1)
 
     # Load Model From disk
-    model = pickle.load(open('model.sav', 'rb'))
+    model = pickle.load(open('../../results/model.sav', 'rb'))
     clf = model['classifier']
     encoder = LabelEncoder()
     encoder.classes_ = model['classes']
